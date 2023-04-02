beff92c0b62d7f02913b7af51e25ea27a3dcd944
John Van Sloun <jpvansloun@gmail.com>
2023-04-02 18:43:36 -0500

    added playable functionality

--- src/Tournament.java
+++ src/Tournament.java
@@ -1,2 +1,36 @@
 public class Tournament {
+
+    public static void main(String[] args) {
+        AI randomAI = new AI();
+        AI randomAI2 = new AI();
+        AI smallAI = new SmallestCardAI();
+        AI bigAI = new BiggestCardAI();
+
+        UnoWarMatch randVSRand = new UnoWarMatch(randomAI, randomAI2);
+        System.out.println(randomAI.toString() + " vs. " + randomAI.toString() + " winrate " + randVSRand.winRate(1000));
+
+        UnoWarMatch randVSSmall = new UnoWarMatch(randomAI, smallAI);
+        System.out.println(randomAI.toString() + " vs. " + smallAI.toString() + " winrate " + randVSSmall.winRate(1000));
+
+        UnoWarMatch randVSBig = new UnoWarMatch(randomAI, bigAI);
+        System.out.println(randomAI.toString() + " vs. " + bigAI.toString() + " winrate " + randVSBig.winRate(1000));
+
+        UnoWarMatch smallVSRand = new UnoWarMatch(smallAI, randomAI);
+        System.out.println(smallAI.toString() + " vs. " + randomAI.toString() + " winrate " + smallVSRand.winRate(1000));
+
+        UnoWarMatch smallVSSmall = new UnoWarMatch(smallAI, smallAI);
+        System.out.println(smallAI.toString() + " vs. " + smallAI.toString() + " winrate " + smallVSSmall.winRate(1000));
+
+        UnoWarMatch smallVSBig = new UnoWarMatch(smallAI, bigAI);
+        System.out.println(smallAI.toString() + " vs. " + bigAI.toString() + " winrate " + smallVSBig.winRate(1000));
+
+        UnoWarMatch bigVSRand = new UnoWarMatch(bigAI, randomAI);
+        System.out.println(bigAI.toString() + " vs. " + randomAI.toString() + " winrate " + bigVSRand.winRate(1000));
+
+        UnoWarMatch bigVSSmall = new UnoWarMatch(bigAI, smallAI);
+        System.out.println(bigAI.toString() + " vs. " + smallAI.toString() + " winrate " + bigVSSmall.winRate(1000));
+
+        UnoWarMatch bigVSBig = new UnoWarMatch(bigAI, bigAI);
+        System.out.println(bigAI.toString() + " vs. " + bigAI.toString() + " winrate " + bigVSBig.winRate(1000));
+    }
 }
--- src/UnoWarMatch.java
+++ src/UnoWarMatch.java
@@ -1,2 +1,98 @@
+import java.sql.SQLFeatureNotSupportedException;
+
 public class UnoWarMatch {
+    private AI ai1;
+    private AI ai2;
+
+    /**
+     *
+     * @param ai1 The ai that will take the role of player 1.
+     * @param ai2 The ai that will take the role of player 2.
+     */
+    public UnoWarMatch(AI ai1, AI ai2) {
+        this.ai1 = ai1;
+        this.ai2 = ai2;
+    }
+
+    /**
+     * Plays a round of UnoWar.
+     * @param deck The deck being used in the game
+     * @param hand1 The hand that takes the first turn.
+     * @param hand2 The hand that takes the successive turn.
+     * @param startHand a boolean denoting which hand plays first (true for hand1 and false for hand2).
+     * @return true if player1 wins and false if player2 wins.
+     */
+    public boolean playRound(Deck deck, Hand hand1, Hand hand2, boolean startHand) {
+        CardPile cardPile = new CardPile(deck.draw());
+        boolean turn = startHand;
+        Card ai1Play;
+        Card ai2Play;
+
+        while (true) {
+            if (turn) {
+                ai1Play = ai1.getPlay(hand1, cardPile);
+                if (ai1Play == null) {
+                    return false;
+                }
+                cardPile.play(ai1Play);
+                hand1.remove(ai1Play);
+                turn = false;
+            } else {
+                ai2Play = ai2.getPlay(hand2, cardPile);
+                if (ai2Play == null) {
+                    return true;
+                }
+                cardPile.play(ai2Play);
+                hand2.remove(ai2Play);
+                turn = true;
+            }
+        }
+    }
+
+    /**
+     * Plays a game of UnoWar until one of the players has one 10 rounds.
+     * @return true if ai1 has won and false if ai2 has won.
+     */
+    public boolean playGame() {
+        Deck deck = new Deck();
+
+        Hand ai1Hand = new Hand(deck, 5);
+        Hand ai2Hand = new Hand(deck, 5);
+
+        int ai1Score = 0;
+        int ai2Score = 0;
+
+        boolean turn = true;
+
+        while ((ai1Score != 10) && (ai2Score != 10)) {
+            if (playRound(deck, ai1Hand, ai2Hand, turn)) {
+                ai1Score++;
+                turn = true;
+            } else {
+                ai2Score++;
+                turn = false;
+            }
+        }
+
+        return (ai1Score == 10);
+    }
+
+    /**
+     * Plays nTrials number of games of UnoWar and finds the win rate of ai1.
+     * @param nTrials the number of times a game should be played.
+     * @return The win rate of ai1 as a double.
+     */
+    public double winRate(int nTrials) {
+        int ai1Wins = 0;
+
+        for (int i = 0; i < nTrials; i++) {
+            if (playGame()) {
+                ai1Wins++;
+            }
+        }
+
+        return ((double)(ai1Wins)) / ((double)(nTrials));
+    }
+
+
 }
